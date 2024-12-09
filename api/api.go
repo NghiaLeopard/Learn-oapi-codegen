5335cecb4e1cdcac6b2e72daea324e4d00ee1a27
DaiNghiadz~ <nghiabeo1605@gmail.com>
2024-12-09 22:28:40 +0700

    swagger

--- api/api.go
+++ api/api.go
@@ -2,8 +2,11 @@ package api
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
+	swagger "github.com/gofiber/swagger"
 )
 
 type API struct{}
@@ -35,6 +38,23 @@ func NewApi() *fiber.App {
 	api := &API{}
 
 	server := fiber.New()
+
+	_, err := GetSwagger()
+
+	if err != nil {
+		log.Println("Swagger error")
+	}
+
+	server.Get("/swagger/*", swagger.HandlerDefault)
+
+	server.Get("/swagger.json", func(c *fiber.Ctx) error {
+		spec, err := GetSwagger()
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).SendString(fmt.Sprintf("Error: %s", err))
+		}
+
+		return c.JSON(spec)
+	})
 
 	handler := NewStrictHandler(api, nil)
 	RegisterHandlers(server, handler)
